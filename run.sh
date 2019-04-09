309a99915d0dd54b0b2cd440b837e1ef55debbff
Ben Johnson <bkj.322@gmail.com>
2019-04-09 15:10:40 -0400

    updating ETL

--- run.sh
+++ run.sh
@@ -5,9 +5,12 @@
 wget https://storage.googleapis.com/nasbench/nasbench_only108.tfrecord
 
 # --
-# Generate graphs
+# Generate arch equivalency classes
 
 python nasbench/scripts/generate-all-graphs.py | gzip -c > data/all-graphs.gz
+
+# --
+# Generate one-exchange neighborhood graph
 
 # !! `all-graphs.gz` has lots of duplicates, so this is ~4x slower than it could be
 #    Takes ~ 10 minutes
@@ -18,3 +21,9 @@ zcat data/all-graphs.gz | \
 
 zcat data/all-edges-nofilter.gz | sort -S50% | uniq | gzip -c > data/tmp.gz
 mv data/tmp.gz data/all-edges-nofilter.gz
+
+# --
+# Clean for export to JHU
+
+# python nasbench/scripts/export-graphs.py
+# ... step through ...
